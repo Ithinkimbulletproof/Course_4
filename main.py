c91924da055877360f5861cc04b30f839aad0629
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-02-06 07:47:34 +0300

    Add main code and json-file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import os
 from abc import ABC, abstractmethod
 
+
 class APIManager(ABC):
 
     @abstractmethod
@@ -11,6 +12,7 @@ class APIManager(ABC):
         """получает вакансии по апи, возвращает список согластно
         запросу пользователя"""
         pass
+
 
 class Vacancy:
     def __init__(self, name, page, top_n):
@@ -21,6 +23,7 @@ class Vacancy:
     def __repr__(self):
         return f"{self.name}"
 
+
 class HeadHunter(Vacancy, APIManager, ABC):
     def __init__(self, name, page, top_n):
         super().__init__(name, page, top_n)
@@ -29,4 +32,45 @@ class HeadHunter(Vacancy, APIManager, ABC):
     def get_vacancies(self):
         """Выгрузка данных по 'HH' по запросам пользователя и
         возвращается словарь"""
-        data = requests.get()
+        data = requests.get(f"{self.url}/vacansies",
+                            params={'text': self.name,
+                                    'page': self.page,
+                                    'per_page': self.top_n}).json()
+        return data
+
+    def load_vacancy(self):
+        """Проходим циклом по словарю берем из словаря только нужные
+        нам данные и записываем их в переменную 'vacancies' """
+        data = self.get_vacancies()
+        vacancies = []
+        for vacancy in data.get('items', []):
+            published_at = datetime.strftime(vacancy['published_at'],
+                                             "%Y-%m-%dT%H:%M:%S%z")
+            vacancy_info = {
+                "id": vacancy['id'],
+                "name": vacancy['name'],
+                "solary_ot": vacancy['salary']['from']
+                if vacancy.get('salary') else None,
+                "solary_do": vacancy['salary']['to']
+                if vacancy.get('salary') else None,
+                "responsibility": vacancy['snippet']
+                ['responsibility'],
+                "data": published_at.strftime("%d.%m.%Y")
+            }
+            vacancies.append(vacancy_info)
+
+        return vacancies
+
+def job_vacancy():
+    """Основной код проекта, после внесения пользователем данных,
+    данные сортируются согласно запросу пользователя и вносятся
+    в json файл"""
+
+    name = input('Введите вакансию: ')
+    top_n = input('Введите кол-во вакансий: ')
+    page = int(input('Введите страницу: '))
+    hh_instance = HeadHunter(name, page, top_n)
+    combined_dict = {'HH': hh_instance.load_vacancy()}
+
+    with open('Found_Vacancies.json', 'w', encoding='utf-8') as file:
+        json.dump(combined_dict, file, ensure_ascii=False, indent=2)
